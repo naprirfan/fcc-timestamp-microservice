59315733c2afd5d363e91d29dd32db391be7fa76
Muhammad Irfan <Muhammad Irfan>
2016-05-17 18:00:27 +0700

    added cors

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var app = express();
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get("/", function(req,res){
    res.render("index");

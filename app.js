42d5cdddcc7612c78db9beba8cdec10c729be518
Muhammad Irfan <Muhammad Irfan>
2016-05-17 15:17:14 +0700

    added bukalapak dummy api

--- app.js
+++ app.js
@@ -44,6 +44,15 @@ app.get("/:time", function(req,res){
 	res.writeHead(200, { 'Content-Type': 'application/json' });   
 	res.end(JSON.stringify(result));
 });
+//dummy api for bukalapak tech test
+app.get("/bukalapak/:id", function(req,res){
+	res.writeHead(200, { 'Content-Type': 'application/json' });   
+	res.end(JSON.stringify({
+		id: req.params.id,
+		stock: Math.floor(Math.random() * 500) + 1,
+		description: "Four dollar toast flexitarian hella, chia fashion axe quinoa chicharrones. Pickled pug DIY, four dollar toast skateboard umami gluten-free brooklyn put a bird on it chartreuse craft beer small batch lomo health goth"
+	}));
+});
 
 app.listen(process.env.PORT || PORT_NUMBER);
 console.log("listening at port " + PORT_NUMBER);
